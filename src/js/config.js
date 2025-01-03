4d7bd8195e01e6b38628d2b02e169022dcd17e1f
TomasUlman <tulman@seznam.cz>
2025-01-03 14:29:35 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6de6fe2f-67f3-4326-ba8a-92b9b0b856da';
+export const MODAL_CLOSE_SEC = 2.5;
